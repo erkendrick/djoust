da55b515c85c0c3d0616b7b1e0a5b44c3a3cc0ad
erkendrick <123228328+erkendrick@users.noreply.github.com>
2024-03-04 11:26:49 -0500

    Add files via upload

---
+++ config/constants.js
@@ -0,0 +1,21 @@
+const RATE = 1000 / 64;
+const GRAVITY = 0.5;
+const canvas = {
+    width: 1280,
+    height: 720
+};
+
+const platforms = [
+    { x: 0, y: 0, width: canvas.width, height: 5 },
+    { x: 0, y: canvas.height - 5, width: canvas.width / 3, height: 5 },
+    { x: canvas.width * 2 / 3, y: canvas.height - 5, width: canvas.width / 3, height: 5 },
+    { x: 0, y: canvas.height / 2, width: canvas.width / 3, height: 25 },
+    { x: canvas.width * 2 / 3, y: canvas.height / 2, width: canvas.width / 3, height: 25 }
+];
+
+export {
+    RATE,
+    GRAVITY,
+    canvas,
+    platforms
+}
